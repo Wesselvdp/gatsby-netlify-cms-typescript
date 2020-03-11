717b92f37f0cf4192b0e49dd6d59512363e575c4
Wessel <wesselvdp@gmail.com>
2020-03-11 19:13:56 +0100

    deployer

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,21 @@
 # Instantly exists our script whenever an error occurs
 set -e
 
-# # Pipe our environmental SSH key variable into a file
+# Pipe our environmental SSH key variable into a file
 mkdir -p $HOME/.ssh
-echo "${DEPLOY_KEY}" > $HOME/.ssh/deploy_key
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-
+echo "${deploy_key}" > $HOME/.ssh/deploy_key
 chmod 600 $HOME/.ssh/deploy_key # SSH keys need to be readonly
 
-# # Where to deploy our site on our server
-target="/var/www/v2"
+# Where to deploy our site on our server
+target="~/html"
 
-# # The actual rsync
-# sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/* --rsync-path='mkdir -p ${target} && rsync' cheil@141.138.139.219:${target}"
+# The actual rsync
+sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/* --rsync-path='mkdir -p ${target} && rsync' <user>@<server>:${target}"
 
-# # Remove our deploy_key again since it's no longer needed
-# rm $HOME/.ssh/deploy_key
+# Remove our deploy_key again since it's no longer needed
+rm $HOME/.ssh/deploy_key
 
-sh -c "scp -v 'ssh -i $HOME/.ssh/deploy_key' -r text.txt cheil@141.138.139.219:/var/www/v2"
+
+# sh -c "scp -v 'ssh -i $HOME/.ssh/deploy_key' -r text.txt cheil@141.138.139.219:/var/www/v2"
 
 # npm run build && scp -r public/* deployer@141.138.139.219:/var/www/v2
