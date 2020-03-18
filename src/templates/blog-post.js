bd27bf43a8a8c5e6dbdecc725c3e2488c2766a6c
Wessel <wesselvdp@gmail.com>
2020-03-18 17:34:47 +0100

    buildfix for preview

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,38 +13,34 @@ class BlogPostTemplate extends React.Component {
     const { previous, next } = this.props.pageContext
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <h1>{post.frontmatter.title}</h1>
-        <p>{post.frontmatter.date}</p>
-        <MDXRenderer>{post.body}</MDXRenderer>
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
+      <h1>Verhaal</h1>
+      // <Layout location={this.props.location} title={siteTitle}>
+      //   <SEO
+      //     title={post.frontmatter.title}
+      //     description={post.frontmatter.description || post.excerpt}
+      //   />
+      //   <h1>{post.frontmatter.title}</h1>
+      //   <p>{post.frontmatter.date}</p>
+      //   <MDXRenderer>{post.body}</MDXRenderer>
+      //   <hr />
 
-        <ul>
-          <li>
-            {previous && (
-              <Link to={`blog${previous.fields.slug}`} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={`blog${next.fields.slug}`} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
+      //   <ul>
+      //     <li>
+      //       {previous && (
+      //         <Link to={`blog${previous.fields.slug}`} rel="prev">
+      //           ← {previous.frontmatter.title}
+      //         </Link>
+      //       )}
+      //     </li>
+      //     <li>
+      //       {next && (
+      //         <Link to={`blog${next.fields.slug}`} rel="next">
+      //           {next.frontmatter.title} →
+      //         </Link>
+      //       )}
+      //     </li>
+      //   </ul>
+      // </Layout>
     )
   }
 }
