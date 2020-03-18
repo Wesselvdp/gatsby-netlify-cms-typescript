647caabf35b379b29e71ff1ae55c0670430270dc
Wessel <wesselvdp@gmail.com>
2020-03-18 17:25:05 +0100

    build fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,36 +11,6 @@ class BlogPostTemplate extends React.Component {
     const post = this.props.data.mdx
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
-
-    return (
-      // <Layout location={this.props.location} title={siteTitle}>
-      //   <SEO
-      //     title={post.frontmatter.title}
-      //     description={post.frontmatter.description || post.excerpt}
-      //   />
-      //   <h1>{post.frontmatter.title}</h1>
-      //   <p>{post.frontmatter.date}</p>
-      //   <MDXRenderer>{post.body}</MDXRenderer>
-      //   <hr />
-
-      //   <ul>
-      //     <li>
-      //       {previous && (
-      //         <Link to={`blog${previous.fields.slug}`} rel="prev">
-      //           ← {previous.frontmatter.title}
-      //         </Link>
-      //       )}
-      //     </li>
-      //     <li>
-      //       {next && (
-      //         <Link to={`blog${next.fields.slug}`} rel="next">
-      //           {next.frontmatter.title} →
-      //         </Link>
-      //       )}
-      //     </li>
-      //   </ul>
-      // </Layout>
-    )
   }
 }
 
